f80c817abb91d082e6c574f20c36fd16947f406f
David Reyes <david.hernandez.r@cua.uam.mx>
2026-03-15 13:59:32 -0600

    Primer commit

---
+++ src/main/java/com/proyecto/lab_tem1/LabTem1Application.java
@@ -0,0 +1,13 @@
+package com.proyecto.lab_tem1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabTem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabTem1Application.class, args);
+	}
+
+}
