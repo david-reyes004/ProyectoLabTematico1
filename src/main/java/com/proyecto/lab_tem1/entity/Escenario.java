d55c403299c8f7a7586c449a588001daad7c7bc3
IvanEmmanuelVazquezLopez.04 <ivan117yobam@gmail.com>
2026-03-21 14:29:23 -0600

    Tablas hechas de Presentaciones y Boletos hecha

---
+++ src/main/java/com/proyecto/lab_tem1/entity/Escenario.java
@@ -0,0 +1,28 @@
+package com.proyecto.lab_tem1.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "escenarios")
+public class Escenario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nombre;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ src/main/java/com/proyecto/lab_tem1/service/BoletoService.java
@@ -0,0 +1,23 @@
+package com.proyecto.lab_tem1.service;
+
+import com.proyecto.lab_tem1.entity.Boleto;
+import com.proyecto.lab_tem1.repository.BoletoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoletoService {
+
+    @Autowired
+    private BoletoRepository boletoRepository;
+
+    public List<Boleto> obtenerTodos() {
+        return boletoRepository.findAll();
+    }
+
+    public Boleto guardar(Boleto boleto) {
+        return boletoRepository.save(boleto);
+    }
+}
